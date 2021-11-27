4ec1aa7f5380fe5584fc635444e56136322c1b5c
Andrei Korikov <kpanic666@gmail.com>
2021-11-27 22:21:53 +0500

    Some refactoring

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ReminderListViewController: UITableViewController {
     static let showDetailSegueIdentifier = "ShowReminderDetailSegue"
+    private var reminderListDataSource: ReminderListDataSource?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        reminderListDataSource = ReminderListDataSource()
+        tableView.dataSource = reminderListDataSource
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == Self.showDetailSegueIdentifier,
@@ -19,33 +26,3 @@ class ReminderListViewController: UITableViewController {
         }
     }
 }
-
-extension ReminderListViewController {
-    static let reminderListCellIdentifier = "ReminderListCell"
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Reminder.testData.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(
-            withIdentifier: Self.reminderListCellIdentifier,
-            for: indexPath
-        ) as? ReminderListCell else {
-            fatalError("Unable to dequeue ReminderCell")
-        }
-        
-        let reminder = Reminder.testData[indexPath.row]
-        let image = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
-        cell.titleLabel.text = reminder.title
-        cell.dateLabel.text = reminder.dueDate.description
-        cell.doneButton.setBackgroundImage(image, for: .normal)
-        cell.doneButtonAction = {
-            Reminder.testData[indexPath.row].isComplete.toggle()
-            tableView.reloadRows(at: [indexPath], with: .none)
-        }
-        
-        return cell
-    }
-}
-
